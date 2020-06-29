af3c63a05c814916437a5749b4f7cb1e19b4b207
Kyle Bennett <kyle.j.bennett5@gmail.com>
2020-06-29 13:41:58 -0400

    npm init and dependencies added

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+    APPLICATION_ID: process.env.APPLICATION_ID || 'WOmOkR3-Afl_gHWRYSNmD0kS_nUqeaBsRMC3C57oqgM',
+    SECRET: process.env.SECRET || 'S1LfoIqHEAkeEqzSLtE7q0v-UrQTHUJBAb1DSwlLYhA',
+    CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+};
