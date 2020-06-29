0542fe04a12de6256274c5bbaf1912e4beba3951
Kyle Bennett <kyle.j.bennett5@gmail.com>
2020-06-29 15:00:33 -0400

    React components and logic

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = {
-    APPLICATION_ID: process.env.APPLICATION_ID || 'WOmOkR3-Afl_gHWRYSNmD0kS_nUqeaBsRMC3C57oqgM',
+    APP_ACCESS_KEY: process.env.APP_ACCESS_KEY || 'WOmOkR3-Afl_gHWRYSNmD0kS_nUqeaBsRMC3C57oqgM',
     SECRET: process.env.SECRET || 'S1LfoIqHEAkeEqzSLtE7q0v-UrQTHUJBAb1DSwlLYhA',
     CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
 };
